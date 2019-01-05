7471f6611931a0a548d6d8801eb474970eca028a
Dominic Crosby <dominiccrosby@Dominics-MacBook.local>
2019-01-05 09:30:30 -0800

    added card and styling

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
+  state = { albums: [] };
   componentWillMount() {
-    console.log('rendered');
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+        .then((response) => response.json())
+        .then((responseData) => this.setState({ albums: responseData }));
+  }
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title} album={album} />
+    );
   }
   render() {
     return (
       <View>
-        <Text>Album List!</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
